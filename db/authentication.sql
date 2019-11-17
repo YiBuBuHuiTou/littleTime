34879cd750a8be20c5581c236d99548ed9fe5297
cxd <591018214@qq.com>
2019-11-17 21:42:24 +0800

    数据库结构更新

--- db/authentication.sql
+++ db/authentication.sql
@@ -16,11 +16,29 @@
 CREATE DATABASE IF NOT EXISTS `authentication` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `authentication`;
 
+-- Dumping structure for table authentication.authentication_histroy
+CREATE TABLE IF NOT EXISTS `authentication_histroy` (
+  `id` bigint(20) unsigned NOT NULL COMMENT '数据库id',
+  `auth_id` int(32) NOT NULL COMMENT '用户ID',
+  `operation` int(1) NOT NULL COMMENT '操作类型',
+  `result` int(1) NOT NULL COMMENT '操作结果',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '操作时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户登录操作历史记录';
+
+-- Data exporting was unselected.
+
 -- Dumping structure for table authentication.user_entry
 CREATE TABLE IF NOT EXISTS `user_entry` (
   `id` int(32) unsigned NOT NULL COMMENT '数据库id、递增',
   `Auth_ID` int(32) unsigned NOT NULL COMMENT '用户ID',
-  `type` int(1) NOT NULL DEFAULT 0 COMMENT '用户查询类型'
+  `type` int(1) NOT NULL DEFAULT 0 COMMENT '用户查询类型；1：加密；0：不加密',
+  `limit` int(1) NOT NULL COMMENT '用户是否冻结',
+  `last_login_time` datetime DEFAULT NULL COMMENT '用户上次登录时间',
+  `locked_time` datetime DEFAULT NULL COMMENT '冻结时间',
+  `login_fail_num` int(1) DEFAULT NULL COMMENT '登录失败次数',
+  `change_nick_name_flag` int(1) NOT NULL COMMENT '是否允许变更nick_name；0：不可； 1 可',
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '数据生成时间',
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '数据更新时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息入口表、用于判断用户登录时的方式：密码登录、无密码登录';
 
 -- Data exporting was unselected.
