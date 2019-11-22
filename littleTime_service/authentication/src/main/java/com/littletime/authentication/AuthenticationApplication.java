5edeb2b997dcafffdbbb897f2e9e95c1330b6d3c
cxd <591018214@qq.com>
2019-11-22 20:18:34 +0800

    简单请求临时保存

--- littleTime_service/authentication/src/main/java/com/littletime/authentication/AuthenticationApplication.java
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/AuthenticationApplication.java
@@ -3,6 +3,9 @@ package com.littletime.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author YiBuBuHuiTou
+ */
 @SpringBootApplication
 public class AuthenticationApplication {
 
---
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
@@ -0,0 +1,18 @@
+package com.littletime.authentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author YiBuBuHuiTou
+ */
+@Controller
+public class UserManagerController {
+
+    @ResponseBody
+    @RequestMapping(value="/hello")
+    public String resiter() {
+        return "success";
+    }
+}
