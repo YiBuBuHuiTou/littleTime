2a617b1a191962311ff49c93505a74557ea816f4
cxd <591018214@qq.com>
2019-11-27 21:02:08 +0800

    自定义配置文件读取 .properties   yml 配置文件读取失败 不知道原因

---
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/config/CustomConfig.java
@@ -0,0 +1,53 @@
+package com.littletime.authentication.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author YiBuBuHuiTou
+ */
+
+@Component
+@PropertySource(value = {"classpath:custom_config.preperties"})
+@ConfigurationProperties(prefix = "custom")
+public class CustomConfig {
+
+    /**
+     * authentication 功能配置
+     */
+    private Map<String, Boolean> sysConfig;
+
+    /**
+     * authencation 其他配置
+     */
+    private Map<String, Boolean> otherConfig;
+
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Map<String, Boolean> getSysConfig() {
+        return sysConfig;
+    }
+
+    public void setSysConfig(Map<String, Boolean> sysConfig) {
+        this.sysConfig = sysConfig;
+    }
+
+    public Map<String, Boolean> getOtherConfig() {
+        return otherConfig;
+    }
+
+    public void setOtherConfig(Map<String, Boolean> otherConfig) {
+        this.otherConfig = otherConfig;
+    }
+}
--- littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
@@ -1,5 +1,7 @@
 package com.littletime.authentication.controller;
 
+import com.littletime.authentication.config.CustomConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,10 +12,15 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
  */
 @Controller
 public class UserManagerController {
+    @Autowired
+    CustomConfig custom;
 
     @ResponseBody
     @RequestMapping(value="/hello")
     public String resiter() {
+        System.out.println("================" + custom.getTest());
+        System.out.println("================" + custom.getSysConfig().get("encryptEnable"));
+
         return "success";
     }
 
