c129148abbe5d8ec1d2c437eb0d26b8264988a81
YiBuBuHuiTou <591018214@qq.com>
2019-11-30 21:15:54 +0800

    过滤器与拦截器实现

---
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/filter/CommonFilter.java
@@ -0,0 +1,32 @@
+package com.littletime.authentication.filter;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @author YiBuBuHuiTou
+ * 共通过滤器
+ * 用来记录请求相关信息 access_log
+ */
+@WebFilter(urlPatterns = {"/*"}, filterName = "commonFilter")
+public class CommonFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("request= " + servletRequest.getParameter("test"));
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("servlet statup.");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("servlet shutdown.");
+    }
+}
