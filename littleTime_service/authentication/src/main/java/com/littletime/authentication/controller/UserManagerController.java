d406a6e512ce535fb26725d26bb7ca20c4366104
YiBuBuHuiTou <591018214@qq.com>
2019-11-30 19:40:04 +0800

    配置文件命名修正

--- littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
@@ -1,6 +1,5 @@
 package com.littletime.authentication.controller;
 
-import com.cxd.littletime.common.util.LogUtils;
 import com.littletime.authentication.config.CustomConfig;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class UserManagerController {
-    @Autowired
-    CustomConfig customConfig;
-    private Logger debuglog = LogUtils.getDebugLogger();
-    private Logger accessLog = LogUtils.getAccessLogger();
     @ResponseBody
     @RequestMapping(value="/hello")
     public String resiter() {
-        System.out.println("================" + customConfig.getTest());
-        System.out.println("================" + customConfig.getSysConfig().get("encryptEnable"));
-        debuglog.debug("debug");
-        accessLog.debug("access");
         return "success";
     }
 
