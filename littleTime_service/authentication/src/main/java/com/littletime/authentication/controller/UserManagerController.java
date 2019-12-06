34c39ecba15dbf90d3e744d582f13c4bf502d436
cxd <591018214@qq.com>
2019-12-06 21:43:11 +0800

    多配置文件启用

--- littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/controller/UserManagerController.java
@@ -1,8 +1,5 @@
 package com.littletime.authentication.controller;
 
-import com.littletime.authentication.config.CustomConfig;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
