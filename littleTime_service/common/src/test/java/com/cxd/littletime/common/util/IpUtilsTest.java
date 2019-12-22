bca4d0dd60d5a6e77268397124cbb8fa18c014b4
YiBuBuHuiTou <591018214@qq.com>
2019-12-22 13:54:17 +0800

    测试代码修正

---
+++ littleTime_service/common/src/test/java/com/cxd/littletime/common/util/IpUtilsTest.java
@@ -0,0 +1,31 @@
+package com.cxd.littletime.common.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IpUtilsTest {
+
+    @Test
+    public void isSingleIP() {
+        String ips = "10.10.10.1-10.10.10.222";
+        String ip = "106.54.198.188";
+        Assert.assertFalse(IpUtils.isSingleIP(ips));
+        Assert.assertTrue(IpUtils.isSingleIP(ip));
+    }
+
+    @Test
+    public void isIP() {
+        String ip = "10.198.457.79";
+        String lp = "546.88.11.255";
+        Assert.assertFalse(IpUtils.isIP(lp));
+        Assert.assertFalse(IpUtils.isIP(ip));
+    }
+
+    @Test
+    public void ip2Long() {
+        String ip = "10.10.10.10";
+        Assert.assertEquals(168430090L,IpUtils.ip2Long(ip));
+    }
+}
---
+++ littleTime_service/common/src/test/java/com/cxd/littletime/common/util/LogUtilsTest.java
@@ -0,0 +1,18 @@
+package com.cxd.littletime.common.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogUtilsTest {
+
+    @Test
+    public void getDebugLogger() {
+        //TODO
+    }
+
+    @Test
+    public void getAccessLogger() {
+        //TODO
+    }
+}
