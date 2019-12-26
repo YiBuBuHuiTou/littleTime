667463d7bc214e985dd2eea29065bf5b970ed94e
cxd <591018214@qq.com>
2019-12-26 21:23:23 +0800

    将拦截器注入到spring中

--- littleTime_service/authentication/src/main/java/com/littletime/authentication/config/InterceptorConfig.java
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/config/InterceptorConfig.java
@@ -1,9 +1,11 @@
 package com.littletime.authentication.config;
 
 import com.littletime.authentication.interceptor.CommonInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,15 @@ import java.util.List;
  */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
+
+    /**
+     * 使用Bean注解将返回的拦截器实例注入spring，否则该拦截器中的自动注入Bean将不起作用
+     * @return  l拦截器实例
+     */
+    @Bean
+    public HandlerInterceptorAdapter getCommonInterceptor() {
+        return new CommonInterceptor();
+    }
     /**
      * 自定义拦截规则
      */
@@ -24,7 +35,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
         List<String> interceptorDisableList = new ArrayList<String>();
         interceptorEnableList.add("/*");
         interceptorDisableList.add("/test");
-        // 注册拦截器规则
-        registry.addInterceptor(new CommonInterceptor()).addPathPatterns(interceptorEnableList).excludePathPatterns(interceptorDisableList);
+        // 注册拦截器规则的同时注入spring ，使用上面的getCommonInterceptor方法获取拦截器实例
+        registry.addInterceptor(getCommonInterceptor()).addPathPatterns(interceptorEnableList).excludePathPatterns(interceptorDisableList);
     }
 }
