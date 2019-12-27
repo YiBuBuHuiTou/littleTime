70eb703e50f462652c41f309921cbcc066ef82fd
cxd <591018214@qq.com>
2019-12-28 00:07:35 +0800

    国际化拦截器追加

--- littleTime_service/authentication/src/main/java/com/littletime/authentication/config/InterceptorConfig.java
+++ littleTime_service/authentication/src/main/java/com/littletime/authentication/config/InterceptorConfig.java
@@ -3,12 +3,16 @@ package com.littletime.authentication.config;
 import com.littletime.authentication.interceptor.CommonInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author YiBuBuHuiTou
@@ -24,6 +28,28 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public HandlerInterceptorAdapter getCommonInterceptor() {
         return new CommonInterceptor();
     }
+
+    /**
+     *
+     * 默认国际化语言 简体中文
+     */
+    @Bean
+    public LocaleResolver  localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.CHINA);
+        return sessionLocaleResolver;
+    }
+
+    /**
+     * 根据 请求的lang参数设置国际化语言
+     * @return
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
     /**
      * 自定义拦截规则
      */
@@ -37,5 +63,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
         interceptorDisableList.add("/test");
         // 注册拦截器规则的同时注入spring ，使用上面的getCommonInterceptor方法获取拦截器实例
         registry.addInterceptor(getCommonInterceptor()).addPathPatterns(interceptorEnableList).excludePathPatterns(interceptorDisableList);
+        //添加国际化拦截器
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
